00b73e08221728a1e7713b626001458ef4e05bb2
Andrej Schmidt <andrej.schmidt98@gmail.com>
2019-03-13 14:06:20 +0100

    vytvorenie store
    vytvorenie store a zlucenie indexovania do jednej metody

--- humoresky_backend/app/Http/Controllers/JokeController.php
+++ humoresky_backend/app/Http/Controllers/JokeController.php
@@ -12,24 +12,14 @@ class JokeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $joke = Joke::all()->orderBy('created_at', 'desc')->paginate(10);
+        if (isset($request->user_id))
+            $jokes = Joke::all()->where('user_id', '==', Auth::id())->orderBy('created_at', 'desc')->paginate(10);
+        else
+            $jokes = Joke::all()->orderBy('created_at', 'desc')->paginate(10);
 
-        return response($joke->toJson(), 200, ['Content-Type' => 'application/json']);
-
-    }
-
-    /**
-     * Display a listing of personal jokes.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index_personal()
-    {
-        $joke = Joke::all()->where('user_id', '==', Auth::id())->orderBy('created_at', 'desc')->paginate(10);
-        
-        return response($joke->toJson(), 200, ['Content-Type' => 'application/json']);
+        return response($jokes->toJson(), 200, ['Content-Type' => 'application/json']);
 
     }
 
@@ -38,9 +28,9 @@ class JokeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+
     }
 
     /**
@@ -51,7 +41,15 @@ class JokeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $joke = new Joke;
+
+        $joke->nazov = $request->nazov;
+        $joke->popis = $request->popis;
+        $joke->telo = $request->telo;
+        $joke->obrazok = $request->obrazok; //bude treba porobic
+        $joke->user_id = Auth::id(); //neni som si isty tymto, asi zle
+
+        $joke->save();
     }
 
     /**
