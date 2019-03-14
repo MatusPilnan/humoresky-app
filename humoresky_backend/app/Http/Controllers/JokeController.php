1ef372bfee1c475b3168b4411f1449cdf7398f0f
Andrej Schmidt <andrej.schmidt98@gmail.com>
2019-03-14 12:22:56 +0100

    nejake drobne upravicky volani

--- humoresky_backend/app/Http/Controllers/JokeController.php
+++ humoresky_backend/app/Http/Controllers/JokeController.php
@@ -20,17 +20,6 @@ class JokeController extends Controller
             $jokes = Joke::all()->orderBy('created_at', 'desc')->paginate(10);
 
         return response($jokes->toJson(), 200, ['Content-Type' => 'application/json']);
-
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(Request $request)
-    {
-
     }
 
     /**
@@ -65,18 +54,6 @@ class JokeController extends Controller
             return response('Vtip nenajdeny. Isla babka po pusti a padol na nu balkon.', 404);
         }
         return response($joke->toJson(), 200, ['Content-Type' => 'application/json']);
-
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Joke  $joke
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Joke $joke)
-    {
-        //
     }
 
     /**
@@ -86,9 +63,17 @@ class JokeController extends Controller
      * @param  \App\Joke  $joke
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Joke $joke)
+    public function update(Request $request)
     {
-        //
+        $joke = Joke::find($request->query('joke'));
+
+        $joke->nazov = $request->nazov;
+        $joke->popis = $request->popis;
+        $joke->telo = $request->telo;
+        $joke->obrazok = $request->obrazok; //bude treba porobic
+        $joke->user_id = Auth::id(); //neni som si isty tymto, asi zle
+
+        $joke->save();
     }
 
     /**
@@ -97,8 +82,10 @@ class JokeController extends Controller
      * @param  \App\Joke  $joke
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Joke $joke)
+    public function destroy()
     {
-        //
+        $joke = Joke::find($request->query('joke'));
+
+        $joke->delete();
     }
 }
