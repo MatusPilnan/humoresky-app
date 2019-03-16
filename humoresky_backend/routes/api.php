7dc7b93e632e185eea9b5fd3f2ed678769e40c1e
Andrej Schmidt <andrej.schmidt98@gmail.com>
2019-03-16 23:07:46 +0100

    pridanie routov do api

--- humoresky_backend/routes/api.php
+++ humoresky_backend/routes/api.php
@@ -17,10 +17,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//zobrazovanie a v indexovanie vtipu
 Route::get('/vtip', 'JokeController@show');
 Route::get('/vtipy', 'JokeController@index');
 
+//prihlasovanie
 Route::post('/login', 'Auth\ApiLoginController@apiLogin');
 Route::post('/register', 'Auth\ApiRegisterController@register');
 
+//manipulacia s vtipmi
+Route::middleware('auth:api')->post('/vtip/uloz', 'JokeController@store');
+Route::middleware('auth:api')->post('/vtip/uprav', 'JokeController@update');
+Route::middleware('auth:api')->delete('/vtip/vymaz', 'JokeController@delete');
 Route::middleware('auth:api')->post('/vtip/hodnotenie', 'JokeController@rate');
