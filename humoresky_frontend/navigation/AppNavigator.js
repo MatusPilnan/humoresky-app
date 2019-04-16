a7d9d1c5de277f70e63dfdfebf5d247010f63e1e
MatusPilnan <m.pilnan@gmail.com>
2019-04-16 13:34:47 +0200

    spravene odhlasenie a zapamatanie prihlasenia

--- humoresky_frontend/navigation/AppNavigator.js
+++ humoresky_frontend/navigation/AppNavigator.js
@@ -3,10 +3,12 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainDrawerNavigator from './MainDrawerNavigator';
 import LoggedInDrawerNavigator from './LoggedInDrawerNavigator'
+import AuthScreen from '../screens/AuthScreen'
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainDrawerNavigator,
+  Main: AuthScreen,
+  Guest: MainDrawerNavigator,
   LoggedIn: LoggedInDrawerNavigator
 }));
